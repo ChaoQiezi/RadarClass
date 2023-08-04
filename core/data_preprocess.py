d25aaeafba34e70c0f293bf25c8f5d32679dee26
ChaoQiezi <chaoqiezi.one@qq.com>
2023-08-04 11:13:35 +0800

    data preprocess
    Signed-off-by: ChaoQiezi <3223758990@qq.com>

--- core/data_preprocess.py
+++ core/data_preprocess.py
@@ -6,17 +6,41 @@
 This script is used to ...
 """
 
+import os
 import yaml
+import numpy as np
+
 from utils.DataManager import DataManager
+from utils.utils import make_chips
 
 # load the config file
 config = yaml.load(open("config.yml", "r", encoding="utf-8"), Loader=yaml.FullLoader)
 
-
 # load and disposal the features and labels dataset
 data_manager = DataManager()
-radar_vvs = data_manager.load_data(config['radar_vv_dir'])
+radar_vvs = data_manager.load_data(config['radar_vv_dir'])  # shape: (time_steps, rows, cols)
 radar_vhs = data_manager.load_data(config['radar_vh_dir'])
+labels, _ = data_manager.read_img(config['labels_path'])  # shape: (rows, cols)
 
-
-
+# nomalize and make chips
+features = np.stack((radar_vvs, radar_vhs), axis=-1)  # shape: (time_steps, rows, cols, bands=2)
+features = data_manager.nomalize(features, train_flag=True)
+features = make_chips(features, (data_manager.time_steps, config['chip_size'], config['chip_size'], 2),
+                      config['stride'])
+"""shape: (n_samples, time_steps, chip_size, chip_size, bands=2)"""
+labels = np.reshape(labels, -1)  # shape: (rows*cols, )
+# discard the invalid samples based on the labels
+valid_index = ~np.isnan(labels)
+features = features[valid_index]
+labels = labels[valid_index]
+# split the dataset
+features_train, features_test, labels_train, labels_test = data_manager.split_data(features, labels)
+# save the dataset
+features_labels = {
+    'features_train': features_train,
+    'features_test': features_test,
+    'labels_train': labels_train,
+    'labels_test': labels_test
+}
+save_path = os.path.join(config['save_dir'], 'features_labels.npy')
+np.save(save_path, features_labels)
--- utils/utils.py
+++ utils/utils.py
@@ -6,12 +6,16 @@
 This script is used to save the helpful tools
 """
 
+import numpy as np
+from skimage.util import view_as_windows
 from osgeo import gdal
 
+
 def read_img(img_path):
-
-
     img = gdal.Open(img_path)
+    if img is None:
+        raise ValueError("The image {} is not exist".format(img_path))
+
     img_cols = img.RasterXSize  # the cols of the image
     img_rows = img.RasterYSize  # the rows of the image
     img_nodata = img.GetRasterBand(1).GetNoDataValue()  # the nodata value of the image
@@ -21,10 +25,18 @@ def read_img(img_path):
     img_projection = img.GetProjection()
 
     # get the bands of the image
-    img_data = img.ReadAsArray(0, 0, img_width, img_height)
+    img_data = img.ReadAsArray(0, 0, img_cols, img_rows)
 
     del img
 
     return img_data, [img_rows, img_cols, img_nodata, img_transform, img_projection]
 
 
+def make_chips(dataset, window_shape, stride=1):
+    features = view_as_windows(dataset, window_shape, stride)
+
+    features = np.squeeze(features)  # shape: (_rows, _cols, time_steps, chip_size, chip_size, bands=2)
+    features = np.reshape(features,
+                          (-1, *features.shape[2:]))  # shape: (n_samples, time_steps, chip_size, chip_size, bands=2)
+
+    return features
